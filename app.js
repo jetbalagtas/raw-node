be15ab6d4d24c168f6c8071ae12d4028cd4d84cb
Jet Balagtas <jetbalagtas@gmail.com>
2019-01-25 13:44:45 -0500

    redirecting requests

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
     res.write('<body><form action="/message" method="POST"><input type="text" name="form-message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY TEXT');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
